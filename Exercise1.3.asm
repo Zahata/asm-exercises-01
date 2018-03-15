6918bbb9831550135551585cdc1ae8998a549878
Zahari Dzhelepov <253925@via.dk>
2018-03-15 08:45:22 +0100

    Exercise1.3

---
+++ Exercise1.3.asm
@@ -0,0 +1,6 @@
+
+
+
+	ldi r16, 42				;puts the decimal value in register 16
+	ldi r17, 0x21			;puts the hexadecimal value 33 in register 17
+	ldi r18, 10110010		;puts the binary value in register 18

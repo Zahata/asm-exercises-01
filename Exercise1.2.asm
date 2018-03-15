cd8794ff28c1558bb75e504331d7567a23585102
Zahari Dzhelepov <253925@via.dk>
2018-03-15 08:44:47 +0100

    Exercise1.2

---
+++ Exercise1.2.asm
@@ -0,0 +1,11 @@
+;
+; AssemblerApplication3.asm
+;
+; Created: 13-Mar-18 10:04:52 AM
+; Author : Zahata97
+;
+;256 kb program memory
+;8 bytes RAM	
+
+ldi r16, 0b0100_0010    ; load value into register r16
+nop                     ; does nothing, but provides a place to put a breakpoint

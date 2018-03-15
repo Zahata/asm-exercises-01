d2820afc11e0021bf90ba16d2905a2a647a07120
Zahari Dzhelepov <253925@via.dk>
2018-03-15 08:58:05 +0100

    Exercise 1.4

---
+++ Exercise1.4.asm
@@ -0,0 +1,12 @@
+;
+; AssemblerApplication4.asm
+;
+; Created: 15-Mar-18 8:53:50 AM
+; Author : Zahata97
+;
+
+
+	ldi		r17,0x08		;load value into register 17
+	ldi		r18,0xb3		;load value into register 18
+	add		r18,r17			;add value r17 into r18, r18 = r18 + r17
+	nop		
